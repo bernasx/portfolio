099d512472c58495dd544a1ba7f585d8228e9450
Bernas <bernasfixolas@hotmail.com>
2022-07-27 14:23:35 +0100

    Removed Blog Nav, fixed switches for very small screens.

--- components/portfolio/lang-switch/index.tsx
+++ components/portfolio/lang-switch/index.tsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { Switch } from '@headlessui/react'
+import useWindowSize from '../../../lib/useWindowSize'
+
 const LangSwitch = () => {
   const { locale, asPath } = useRouter();
   const [enabled, setEnabled] = useState(locale === 'en' ? false : true)
   const router = useRouter();
-
+  const {width} = useWindowSize() // we have to use this here because tailwind does not support breakpoints for translate
 
   useEffect(()=> {
     const goToLocale = (newLocale:string) => {
       document.cookie=`NEXT_LOCALE=${newLocale}; expires=Fri, 31 Dec 9999 23:59:59 GMT`
       router.push(asPath, asPath, { locale: newLocale })
     }
-    
     if(enabled){
       // PT
       goToLocale('pt');
@@ -20,23 +21,30 @@ const LangSwitch = () => {
       // EN
       goToLocale('en');
     }
-    
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[enabled])
 
+    let switchClass = `${enabled ? 'bg-blue-600' : 'bg-gray-200'} relative inline-flex h-8 w-16 items-center rounded-md bg-port-superlight-green dark:bg-port-darker-blue`
+    let switchIconClass =  `${enabled ? 'translate-x-8' : 'translate-x-1'} inline-block h-7 w-7 transform rounded-md bg-port-light-green`
+
+    if(width! < 375) {
+      switchClass = `${enabled ? 'bg-blue-600' : 'bg-gray-200'} relative inline-flex h-7 w-14 items-center rounded-md bg-port-superlight-green dark:bg-port-darker-blue`
+      switchIconClass =  `${enabled ? 'translate-x-7' : 'translate-x-1'} inline-block h-6 w-6 transform rounded-md bg-port-light-green`
+    }
 
     return <Switch
       checked={enabled}
       onChange={setEnabled}
-      className={`${enabled ? 'bg-blue-600' : 'bg-gray-200'
-        }  relative inline-flex h-8 w-16 items-center rounded-md bg-port-superlight-green dark:bg-port-darker-blue`}
-    >
-      <span className="sr-only">Enable notifications</span>
+      className={switchClass}
+      >
+
+      <span className="sr-only">Switch Language</span>
+
       <span
-        className={`${enabled ? 'translate-x-8' : 'translate-x-1'
-          } inline-block h-7 w-7 transform rounded-md bg-port-light-green`}
+        className={switchIconClass}
       > <div className='flex items-center justify-center w-full h-full text-lg text-white'>{enabled ? 'PT' : 'EN'}</div>
       </span>
+
     </Switch>
   }
 
--- components/portfolio/theme-switch/index.tsx
+++ components/portfolio/theme-switch/index.tsx
@@ -3,11 +3,13 @@ import { Switch } from '@headlessui/react'
 import { MoonIcon } from '@heroicons/react/solid'
 import { SunIcon } from '@heroicons/react/solid'
 import {useTheme} from 'next-themes'
+import useWindowSize from '../../../lib/useWindowSize'
 
 const ThemeSwitch = () => {
   const { theme, setTheme } = useTheme()
   const [enabled, setEnabled] = useState(theme == 'light' ? false : true)
   const [mounted, setMounted] = useState(false)
+  const {width} = useWindowSize() // we have to use this here because tailwind does not support breakpoints for translate
 
   // useEffect only runs on the client, so now we can safely show the UI
   useEffect(() => {
@@ -25,16 +27,24 @@ const ThemeSwitch = () => {
   if (!mounted) {
     return null
   }
+
+  let switchClass = `${enabled ? 'bg-blue-600' : 'bg-gray-200'} relative inline-flex h-8 w-16 items-center rounded-md bg-port-superlight-green dark:bg-port-darker-blue`
+  let switchIconClass =  `${enabled ? 'translate-x-8' : 'translate-x-1'} inline-block h-7 w-7 transform rounded-md bg-port-light-green`
+
+  if(width! < 375) {
+    switchClass = `${enabled ? 'bg-blue-600' : 'bg-gray-200'} relative inline-flex h-7 w-14 items-center rounded-md bg-port-superlight-green dark:bg-port-darker-blue`
+    switchIconClass =  `${enabled ? 'translate-x-7' : 'translate-x-1'} inline-block h-6 w-6 transform rounded-md bg-port-light-green`
+  }
+
+
     return <Switch
       checked={enabled}
       onChange={setEnabled}
-      className={`${enabled ? 'bg-blue-600' : 'bg-gray-200'
-        } relative inline-flex h-8 w-16 items-center rounded-md bg-port-superlight-green dark:bg-port-darker-blue`}
+      className={switchClass}
     >
       <span className="sr-only">Enable notifications</span>
       <span
-        className={`${enabled ? 'translate-x-8' : 'translate-x-1'
-          } inline-block h-7 w-7 transform rounded-md bg-port-light-green`}
+        className={switchIconClass}
       >{enabled ? <MoonIcon color='white'></MoonIcon> : <SunIcon color='white'></SunIcon>}
       </span>
     </Switch>
